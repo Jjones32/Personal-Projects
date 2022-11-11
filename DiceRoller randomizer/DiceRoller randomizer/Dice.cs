e8a15c5b88a16378cc5dcbbf42c05612a704207b
Finesstro <jake_joe22@yahoo.com>
2022-11-11 11:46:26 -0500

    Dice Randomizer application dealing with random

---
+++ DiceRoller randomizer/DiceRoller randomizer/Dice.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceRoller_randomizer
+{
+    public class Dice
+    {
+        private static readonly Random _random = new Random();
+
+        public static int RollDice (int diceSides)
+        {
+            int rollDice = _random.Next(1, diceSides + 1);
+            return rollDice;
+        }
+
+        public static string CheckCombos(int die1, int die2)
+        {
+            string combo = "";
+
+            if (Dice.CheckSnakeEyes(die1, die2))
+            {
+                combo= "Snake Eyes";
+            }
+            else if (Dice.CheckSnakeEyes(die1, die2))
+            {
+                combo = "Ace Deuce";
+            }
+            else if (Dice.CheckBoxCars(die1, die2)) ;
+            {
+                combo = "Box Cars";
+            }
+            return combo;
+        }
+        public static string CheckCombos(int total)
+        {
+            string combo = "";
+            if (Dice.CheckWin(total))
+            {
+                combo = "Win!";
+            }
+            else if (Dice.CheckCraps(total))
+            {
+                combo = "craps!";
+            }
+            return combo;
+        }
+        public static bool CheckSnakeEyes(int die1, int die2)
+        {
+            return (die1 == 1 && die2 == 1);
+        }
+        public static bool CheckAceDeuce(int die1, int die2)
+        {
+            return ((die1 == 1 && die2 == 2) || (die1 == 2 && die2 == 1)); 
+        }
+        public static bool CheckBoxCars(int die1, int die2)
+        {
+            return (die1 == 6 && die2 == 6);
+        }
+        public static bool CheckWin (int total)
+        {
+            return (total == 7 || total == 11);
+        }
+        public static bool CheckCraps(int total)
+        {
+            return (total == 2 || total == 3 || total == 12);
+        }
+    }
+}
---
+++ DiceRoller randomizer/DiceRoller randomizer/Program.cs
@@ -0,0 +1,49 @@
+﻿
+using DiceRoller_randomizer;
+
+string yn = "y";
+do
+{
+    Console.Write("Enter number of sides for each dice and roll: ");
+    int numbSidesDice = int.Parse(Console.ReadLine());
+
+    int die1 = Dice.RollDice(numbSidesDice);
+    int die2 = Dice.RollDice(numbSidesDice);
+    int total = die1 + die2;
+    Console.WriteLine($"Dice 1: {die1}");
+    Console.WriteLine($"Dice 2: {die2}");
+    Console.WriteLine($"Total for Roll: {total}");
+
+    if (numbSidesDice == 6)
+    {
+        Console.WriteLine(Dice.CheckCombos(die1, die2));
+        Console.WriteLine(Dice.CheckCombos(total));
+    }
+    Console.Write("Do you want to roll again? Enter y or n: ");
+    yn = Console.ReadLine().ToLower();
+}
+while (yn == "y");
+
+
+
+
+
+
+
+
+
+
+
+
+
+//1.)Create a static method to generate the random numbers 
+//          a.) Proper method header
+//          b.) Program generates random numbers validly within the user-specified range
+//          c.) Method returns meanigful value of proper type
+//2.)Create a static method for six-sided dice that takes two dice values as parameters, and returns a string for one of the valid combinations (e.g. Snake Eyes, etc.) or an empty sting if the dice doesn't match one of the combinations
+//3.)Create a static method to implement output for different dice combinations
+//          a.) Proper Method Header
+//          b.) Method recognizes all specified cases correctly
+//          c.) Method displays properly to console
+//4.)App takes all user input correctly
+//5.)Application loops properly
